809d62fbbd94800c43ff3ba4f4fd951f6291fad8
swissbison <psig@gmx.ch>
2016-09-17 23:28:48 +0200

    Added more & better tests

---
+++ test/Worklog_Test10.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    ManualTestCase
+#>
+$TestCase = "Worklog_Test2.ps1"
+Write-Host "Testcase: $TestCase" -ForegroundColor Yellow
+
+$TestDir = $PSScriptRoot
+
+$ProjDir = Split-Path -Path $TestDir -Parent
+
+. $ProjDir\Load-Worklog.ps1
+
+$WorklogFile = "$TestDir\test2-worklog.txt"
+
+$Lines = Show-WorklogReport -GroupingProperty Date -CustomWorklogFile $WorklogFile
+
+$Lines
+
+if($Lines.Count -eq 13) {
+    Write-Host "$TestCase OK" -ForegroundColor Green
+} else {
+    Write-Host "$TestCase Failed" -ForegroundColor Red
+}
---
+++ test/Worklog_Test3.ps1
@@ -0,0 +1,34 @@
+﻿<#
+    ManualTestCase
+#>
+$TestCase = "Worklog_Test3.ps1"
+Write-Host "Testcase: $TestCase" -ForegroundColor Yellow
+
+$TestDir = $PSScriptRoot
+
+$ProjDir = Split-Path -Path $TestDir -Parent
+
+
+. $ProjDir\Load-Worklog.ps1
+
+
+$WorklogFile = "$TestDir\test3-worklog.txt"
+if(Test-Path $WorklogFile) {Remove-Item -Path $WorklogFile}
+
+Add-WorklogLine '02:00 code itsm this is a comment1' -CustomWorklogFile $WorklogFile
+Add-WorklogLine '03:00 mgmt itshop this is a comment2' -CustomWorklogFile $WorklogFile
+Add-WorklogLine '01:00 mgmt itshop OTRS#ID#1000001 this is a comment2' -CustomWorklogFile $WorklogFile
+Add-WorklogLine 'mgmt itshop this is a comment2' -CustomWorklogFile $WorklogFile
+Add-WorklogLine 'code itsm this is a comment1' -CustomWorklogFile $WorklogFile
+Add-WorklogLine 'spec itsm KB#10002 this is a comment1' -CustomWorklogFile $WorklogFile
+Add-WorklogLine 'off' -CustomWorklogFile $WorklogFile
+
+$Lines = Get-Content $WorklogFile
+
+$Lines
+
+if($Lines.Count -eq 15) {
+    Write-Host "$TestCase OK" -ForegroundColor Green
+} else {
+    Write-Host "$TestCase Failed" -ForegroundColor Red
+}
